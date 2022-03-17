17113725a7216b49fbf777dc43829847c3298850
Phillip Truong <TruongHPhillip@gmail.com>
2022-03-17 00:42:55 -0400

    added some basic navigation

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Home from './src/pages/home';
+import DetailsScreen from './src/pages/detail';
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Hello World :)</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused
+              ? 'ios-home'
+              : 'ios-home';
+          } else if (route.name === 'DetailsScreen') {
+            iconName = focused ? 'ios-list' : 'ios-list';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="DetailsScreen" component={DetailsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 }
 
--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 
-const Home = () => {
+const DetailsScreen = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text>Details Screen</Text>
     </View>
-  )
+  );
 }
 
-export default Home
+export default DetailsScreen
