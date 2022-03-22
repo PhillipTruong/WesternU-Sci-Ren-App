470a54a28d6cc4cd3f7ef806bd2874b760769159
Phillip Truong <TruongHPhillip@gmail.com>
2022-03-22 11:53:10 -0400

    added title to map page

--- pages/map.js
+++ pages/map.js
@@ -1,19 +1,29 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
 
 const Map = () => {
   return (
-    <View style={styles.container}>
-      <Text>Map Screen</Text>
-    </View>
+    <SafeAreaView style={styles.safeAreaViewContainer}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Map Screen</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeAreaViewContainer: {
+    flex: 1,
+    paddingVertical: 40,
+    backgroundColor: '#EFFFFD',
+  },
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#EFFFFD'
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
   },
 });
 
