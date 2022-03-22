40800423194547277c1802e933b1a6875a163186
Phillip Truong <TruongHPhillip@gmail.com>
2022-03-22 11:10:12 -0400

    added Faq Data

---
+++ helpers/faqData.js
@@ -0,0 +1,18 @@
+
+export const faqData = [
+  {
+    key: 1, question: 'What is Science Rendezvous?', answer: 'Science Rendezvous is a nation-wide initiative, organizing an annual all day festival that brings science out of the labs and into the community. It occurs simultaneously at over 40 sites across 30 Canadian cities! Science Rendezvous\' goal is to increase visibility of, and public interest in science, technology, engineering, arts, and mathematics (S.T.E.A.M.) fields.'
+  },
+  {
+    key: 2, question: 'Who can attend?', answer: 'All individuals (regardless of age) are encouraged to attend and learn about the mysteries of science. Bring your curiosity and questions to Science Rendezvous and ask our scientific experts at the event!'
+  },
+  {
+    key: 3, question: 'Is the event free?', answer: 'Absolutely! There is no cost to this event. You are welcome to stay for 5 minutes, 10 minutes, 1 hour or join us for the whole day!'
+  },
+  {
+    key: 4, question: 'What can I expect to see and do?', answer: 'Come and visit our 30+ unique displays of S.T.E.A.M. fields! There will be demonstrations, a stage show, interactive activities, build-and-take-home projects, and more! There is so much to learn and see.'
+  },
+  {
+    key: 5, question: 'How will COVID-19 restrictions impact Science Rendezvous 2022?', answer: 'In an effort to make this a safe event for everyone involved, Science Rendezvous will be working closely with the London Middlesex Health Unit and following local COVID-19 regulations. Attendees and volunteers must adhere to local regulations from the London Middlesex health unit when participating in activities leading up to and including the event on May 7th, 2022. This includes proof of full vaccination and wearing a mask when social distancing cannot be guaranteed.'
+  },
+]
